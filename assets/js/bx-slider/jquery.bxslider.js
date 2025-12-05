78372e8054dc53534efa27f5c284dee46d6aca3d
RashFrank <rashfrank123@gmail.com>
2025-12-05 11:27:44 +0500

    text

--- assets/js/bx-slider/jquery.bxslider.js
+++ assets/js/bx-slider/jquery.bxslider.js
@@ -1,9 +1,4 @@
-/**
- * bxSlider v4.2.12
- * Copyright 2013-2015 Steven Wanderski
- * Written while drinking Belgian ales and listening to jazz
- * Licensed under MIT (http://opensource.org/licenses/MIT)
- */
+
 
 ;(function($) {
 
